d251f96e4c1d79edf12572c343b7be796b593bc5
kkozlovsky <kkozlovsky@yandex.ru>
2024-03-28 09:19:42 +0300

    TransactionCommandService, TransactionQueryService implementation

---
+++ src/main/java/ru/kerporation/cqrsbankingapp/web/dto/mapper/Mappable.java
@@ -0,0 +1,13 @@
+package ru.kerporation.cqrsbankingapp.web.dto.mapper;
+
+import java.util.List;
+
+public interface Mappable<E, D> {
+
+    E fromDto(final D dto);
+
+    D toDto(final E entity);
+
+    List<D> toDto(final List<E> entities);
+
+}
